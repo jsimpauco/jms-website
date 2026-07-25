c8518ba7521ff997aecbbf711227b19dc88e788a
jsimpauco <jms.simpauco@gmail.com>
2026-07-24 19:53:25 -0700

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </div>
         <div>
           <h1>Get started</h1>
+          <h2>TEST</h2>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
